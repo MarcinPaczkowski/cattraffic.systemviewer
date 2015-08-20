001a84d5cd64ccee810d6bd087dc6b5134430547
binaryAdam <aim.kruk@gmail.com>
2015-08-20 20:26:34 +0200

    gowno z SLN

--- CatTraffic.SystemViewer.TcpTestClient/Program.cs
+++ CatTraffic.SystemViewer.TcpTestClient/Program.cs
@@ -16,7 +16,7 @@ namespace CatTraffic.SystemViewer.TcpTestClient
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
